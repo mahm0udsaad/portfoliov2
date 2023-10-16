a46419e602ace588ab6c58970d754ebc8fe71109
unknown <saad123mn123@gmail.com>
2023-10-16 10:17:02 +0300

    update

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,21 @@
-"use client"
 import NavBar from '@/components/navBar'
 import './globals.css'
-import { AnimatePresence, motion } from 'framer-motion'
 import Footer from '@/components/footer';
+import Transition from '@/components/transition';
 
 export const metadata = {
   title: `Mahmoud's Portfolio`,
 }
  
 export default function RootLayout({ children }) {
-  const variants = {
-    hidden: { opacity: 0, x: 200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 1, x: 0, y: 100 },
-}
+
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className='bg-[#121924] overflow-x-hidden'>
         <NavBar />
-          <AnimatePresence >
-          <motion.main
-          onLoad={()=> console.log('loading')}
-          variants={variants} // Pass the variant object into Framer Motion 
-          initial="hidden" // Set the initial state to variants.hidden
-          animate="enter" // Animated state to variants.enter
-          exit="exit" // Exit state (used later) to variants.exit
-          transition={{ type: 'linear' }} // Set the transition to linear
-          >
-              {children}
-          </motion.main>
-          </AnimatePresence>
+          <Transition>
+            {children}
+          </Transition>
           <Footer />
         </body>
     </html>
---
+++ components/transition.jsx
@@ -0,0 +1,25 @@
+"use client"
+import { AnimatePresence, motion } from 'framer-motion'
+
+const Transition = ({children}) =>{
+    const variants = {
+        hidden: { opacity: 0, x: 200, y: 0 },
+        enter: { opacity: 1, x: 0, y: 0 },
+        exit: { opacity: 1, x: 0, y: 100 },
+    }
+    return(
+        <AnimatePresence >
+          <motion.main
+          onLoad={()=> console.log('loading')}
+          variants={variants} // Pass the variant object into Framer Motion 
+          initial="hidden" // Set the initial state to variants.hidden
+          animate="enter" // Animated state to variants.enter
+          exit="exit" // Exit state (used later) to variants.exit
+          transition={{ type: 'linear' }} // Set the transition to linear
+          >
+              {children}
+          </motion.main>
+          </AnimatePresence>
+    )
+}
+export default Transition ;
